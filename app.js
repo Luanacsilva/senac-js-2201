6a17dee6466a6669438a4d764964bb3eb730229b
unknown <Administrador@CAS493131W1016.senacsp.edu.br>
2022-02-16 23:11:29 -0200

    Final da aula

---
+++ app.js
@@ -0,0 +1,38 @@
+// comentario de linha
+
+/*
+comentario de bloco
+*/
+
+const XPTO = 10;
+
+console.log(XPTO);
+
+let minhaVar = 'Olá';
+
+minhaVar = minhaVar + 'Mundo!';
+
+console.log(minhaVar);
+
+ function condLooping(){
+
+    if (true){
+
+    console.log('if verdadeiro');
+    
+    } else {
+    console.log('If falso');
+    }
+
+
+    let cont =1;
+    while(cont<XPTO){
+    console.log("parabens");
+    cont++
+    }
+ }
+ condLooping();
+    
+function diretoNoElemento(){
+    console.log('Olá mundo!');
+}
