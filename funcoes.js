c6ed0bcd89e9110311416b75beaa69d6732782f2
Luana Cristina da Silva <lucris_231@hotmail.com>
2022-03-02 22:19:48 -0300

    Funções e começo do DOM

---
+++ funcoes.js
@@ -0,0 +1,61 @@
+function olaMundoNoLog(nome){
+    console.log('Olá ${nome} !');
+}
+
+const SOMA = function(a, b){
+
+    return a + b 
+};
+
+(function(a, b){
+ console.log(a + b);
+})(3, 3);
+
+olaMundoNoLog('Luana');
+
+let resultado = SOMA(2,2);
+console.log(resultado);
+
+
+let vetor = ['Luana' , 'potato' , '555'];
+
+]function pegaUltimo(vet){
+    let ult = ver.length -1;
+    return vet[ult];
+}
+
+let novoVetor = pegaUltimo(vetor);
+
+console.log('ultimo elemento =' + novoVetor);
+
+let Aluno ={
+    nome:"Amanda",
+    matricula: 54876
+    setNome: function
+}
+Aluno.setNome('blablabla');
+
+let hoje = new Date;
+console.log( hoje.getMonth() +1);
+
+
+function nomeMesCorrente(){
+const MES = [
+    'janeiro',
+    'fevereiro',
+    'março',
+    'abril',
+    'maio',
+    'junho',
+    'julho',
+    'agosto',
+    'setembro',
+    'outubro',
+    'novembro',
+    'dezembro'];
+};
+ let atual = new Date;
+ return MES[atual.getMonth()]
+console.log(nomeMesCorrente());
+console.log(new Date);
+
